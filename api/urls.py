d4ceb8a3a7fd28101f7839f4f0aaa485bc904082
Sofía Rivas Muñoz <sofi.rivasm@gmail.com>
2016-05-10 20:20:47 -0500

    commit sofi

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import IndexView, GreetView, JsonView, ColorView, ColorsView
+from .views import IndexView, GreetView, JsonView, ColorView, ColorsView,FormularioView
 from django.views.decorators.csrf import csrf_exempt
 
 
@@ -8,7 +8,9 @@ urlpatterns = [
     url(r'greet/([a-z]+)$', csrf_exempt(GreetView.as_view())),
     url(r'json/$', csrf_exempt(JsonView.as_view())),
     url(r'color/([a-z]+)$', csrf_exempt(ColorView.as_view())),
-    url(r'colors/$', ColorsView.as_view()),
-
+    url(r'colors/$', ColorsView.as_view(),
+    	name='colors'
+    	),
+    url(r'formulario/$', FormularioView.as_view())
 
 ]
